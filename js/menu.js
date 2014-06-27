759c743552fd45408aa53d5735fee9646330a021
Tiffany Tse <ttse@coldfrontlabs.ca>
2014-06-26 20:41:42 -0700

    Added styles for header and navigation

--- js/menu.js
+++ js/menu.js
@@ -4,8 +4,10 @@
       $('#open_secondary-menu').click(function() {
         if ($('#secondary-menu').attr('data-state') == 'unselected') {
           $('#secondary-menu').attr('data-state', 'selected');
+          $('#page').attr('data-state', 'slide-open');
         } else {
           $('#secondary-menu').attr('data-state', 'unselected');
+          $('#page').attr('data-state', 'slide-closed');
         }
       }); 
     }
